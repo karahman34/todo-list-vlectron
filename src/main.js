f68a3a02ecbf70e7b7f4386abef06a524cd2c574
karahman <jakarahman007@gmail.com>
2021-02-11 21:10:05 +0700

    add toasted package

--- src/main.js
+++ src/main.js
@@ -5,8 +5,17 @@ import store from './store'
 
 import 'tailwindcss/tailwind.css'
 import '@mdi/font/css/materialdesignicons.min.css'
+import Toasted from 'vue-toasted'
 
 Vue.config.productionTip = false
+
+Vue.use(Toasted, {
+  iconPack: 'mdi',
+  position: 'bottom-center',
+  duration: '4500',
+  theme: 'bubble',
+  containerClass: 'font-semibold'
+})
 
 new Vue({
   router,
