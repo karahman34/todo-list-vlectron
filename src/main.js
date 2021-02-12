e74820b42bba8bdd52ad6bd99ea21fb0001a2275
karahman <jakarahman007@gmail.com>
2021-02-12 08:48:10 +0700

    add aos

--- src/main.js
+++ src/main.js
@@ -5,9 +5,13 @@ import store from './store'
 
 import 'tailwindcss/tailwind.css'
 import '@mdi/font/css/materialdesignicons.min.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import Toasted from 'vue-toasted'
 
 Vue.config.productionTip = false
+
+AOS.init()
 
 Vue.use(Toasted, {
   iconPack: 'mdi',
